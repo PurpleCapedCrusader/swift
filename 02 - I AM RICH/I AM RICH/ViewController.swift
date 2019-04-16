49c7b944b4965a4eae435d4aff6cf217e4a47153
PurpleCapedCrusader <cot15011@byui.edu>
2019-04-16 15:21:19 -0700

    02 - I AM RICH

---
+++ 02 - I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Administrator on 4/10/19.
+//  Copyright © 2019 Alex Cotton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
