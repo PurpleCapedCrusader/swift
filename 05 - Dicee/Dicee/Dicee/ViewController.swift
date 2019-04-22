a29f5f774ad0fd3a1b734aa3a80ad42b99551372
Alex <cot15011@byui.edu>
2019-04-22 00:34:37 -0700

    added 05 - dicee, just about to start lesson 48

---
+++ 05 - Dicee/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Alex Cotton on 4/21/19.
+//  Copyright © 2019 AlexCotton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+    }
+    
+    
+}
+
